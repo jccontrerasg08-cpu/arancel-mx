b2bf06859993f1f461e06ade9259bb4fa90f0142
Juca8 <jccontrerasg08@gmail.com>
2026-08-10 15:08:53 -0700

    test: expose DuckDB certification entrypoint

--- src/arancel_mx/certification/__init__.py
+++ src/arancel_mx/certification/__init__.py
@@ -1,6 +1,7 @@
 """Independent read-only checks for certifying public release artifacts."""
 
 from .bundle import certify_bundle
+from .consumer import certify_duckdb
 from .reports import CertificationReport
 
-__all__ = ["CertificationReport", "certify_bundle"]
+__all__ = ["CertificationReport", "certify_bundle", "certify_duckdb"]
