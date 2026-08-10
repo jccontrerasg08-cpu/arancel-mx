b62d00ce38fc78860abd8d8688224e4cb4618224
Juca8 <jccontrerasg08@gmail.com>
2026-08-10 05:16:51 -0700

    feat: export required DOF evidence helpers

--- src/arancel_mx/sources/__init__.py
+++ src/arancel_mx/sources/__init__.py
@@ -3,6 +3,7 @@
 from arancel_mx.sources.capture import CaptureManifest, can_reuse_parse, capture_document
 from arancel_mx.sources.diputados import parse_ligie_ledger
 from arancel_mx.sources.http import FetchedDocument, fetch_official_document
+from arancel_mx.sources.legal_evidence import RequiredDofEvidence, required_dof_evidence
 from arancel_mx.sources.registry import RegistryEntry, classify_candidate, load_source_registry
 from arancel_mx.sources.snice import DownloadTask, discover_snice_documents
 
@@ -11,6 +12,7 @@ __all__ = [
     "DownloadTask",
     "FetchedDocument",
     "RegistryEntry",
+    "RequiredDofEvidence",
     "can_reuse_parse",
     "capture_document",
     "classify_candidate",
@@ -18,4 +20,5 @@ __all__ = [
     "fetch_official_document",
     "load_source_registry",
     "parse_ligie_ledger",
+    "required_dof_evidence",
 ]
