2d8b101708c4647a4706ffec479c4a73f03660ad
Juca8 <jccontrerasg08@gmail.com>
2026-08-10 02:02:08 -0700

    feat: export hierarchy assembly

--- src/arancel_mx/pipeline/__init__.py
+++ src/arancel_mx/pipeline/__init__.py
@@ -1,10 +1,12 @@
 """Build and reconciliation workflows for tariff releases."""
 
 from .build import export_arancel_release, materialize_arancel
+from .hierarchy import assemble_classifications
 from .reconcile import reconcile_legal_instruments
 from .update import check_for_updates, run_update, update_status
 
 __all__ = [
+    "assemble_classifications",
     "export_arancel_release",
     "materialize_arancel",
     "reconcile_legal_instruments",
